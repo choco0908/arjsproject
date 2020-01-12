257dd4219ee422edba4ee05841302f60fc9c21df
choco0908 <starfish0908@naver.com>
2020-01-12 17:50:52 +0900

    Change Nodejs Code

--- artest.js
+++ artest.js
@@ -2,30 +2,37 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 const port = 3000
+
 //const hostname = '192.168.43.137'
-const hostname = '192.168.0.123'
+//const hostname = '192.168.0.123'
+const hostname = '192.168.1.33'
+
 const https = require('https')
+const http = require('http')
 
-app.listen(port,hostname,() => console.log(`Example app listening on https://${hostname}!`))
+app.use(express.static('src'));
 
-var options = {
-    key : fs.readFileSync('serverpem/private.pem'),
-    cert : fs.readFileSync('serverpem/public.pem')
+var options_http = {
+    hostname: hostname,
+    path: '/',
+    method: 'GET',
 }
 
-var https_server = https.createServer( options,(req,res) => {
-    fs.readFile('artest.html',function(error,data){
-        if(error){
-            console.log(error);
-        }
-        else{
-            res.writeHead(200);
-            res.end(data);
-        }
-    })
-}).listen(443);
+var options_https = {
+    hostname: hostname,
+    path: '/',
+    method: 'GET',
+    key : fs.readFileSync('serverpem/private.pem'),
+    cert : fs.readFileSync('serverpem/public.pem'),
+}
 
-app.get('/', function(req, res){
+//http server on
+var http_server = http.createServer(options_http,app).listen(3000);
+
+//https server on
+var https_server = https.createServer( options_https,app ).listen(443);
+
+app.get('/artest', function(req, res){
     fs.readFile('artest.html',function(error,data){
         if(error){
             console.log(error);
